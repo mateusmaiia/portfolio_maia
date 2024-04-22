39f784fc1435d655c48f74e69242fab187369d47
maia <matteusmaia889@gmail.com>
2024-04-22 18:41:54 -0300

    feat: main comp import

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import '../style.css';
 import { Header } from './components/Header/ Header';
+import { Main } from './components/Main/Main';
 
 export function App() {
    return (
       <>
          <Header />
+         <Main />
       </>
    );
 }
