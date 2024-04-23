5e1217a72d6ed51cc4aa455e69ba7f9d37d614b2
maia <matteusmaia889@gmail.com>
2024-04-23 17:38:21 -0300

    feat: index.js to export images

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import Project1 from './img/project1.jpg';
+import Project2 from './img/project2.jpg';
+import Project3 from './img/project3.jpg';
+import Project4 from './img/project4.jpg';
+import perfil from './img/perfil.png';
+
+const images = {
+   Project1,
+   Project2,
+   Project3,
+   Project4,
+   perfil,
+};
+
+export default images;
