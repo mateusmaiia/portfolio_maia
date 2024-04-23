552612061e83dce74a3ad1ff013517ea5f8e2e2f
maia <matteusmaia889@gmail.com>
2024-04-23 17:38:46 -0300

    feat: comp Main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,12 @@
+import Project1 from '../../assets/img/project1.jpg';
+import Project2 from '../../assets/img/project2.jpg';
+import Project3 from '../../assets/img/project3.jpg';
+import Project4 from '../../assets/img/project4.jpg';
+
+import './styles.css';
+
 export function Main() {
+   // const { Project1, Project2, Project3, Project4 } = images;
    return (
       <main className="main">
          <section className="filters container">
@@ -11,7 +19,201 @@ export function Main() {
                   Skills
                </button>
             </ul>
-            <div className="filters__sections"></div>
+            <div className="filters__sections">
+               {/* projects */}
+               <div
+                  data-content
+                  className="projects__content grid filters__active"
+                  id="projects"
+               >
+                  <article className="projects__card">
+                     <img src={Project1} alt="" />
+                     <div className="projects__modal">
+                        <div>
+                           <span className="projects__subtitle">Web</span>
+                           <h3 className="projects__title">Payment Site</h3>
+                           <a
+                              href="#"
+                              className="projects__button button button__small"
+                           >
+                              <i className="ri-link"></i>
+                           </a>
+                        </div>
+                     </div>
+                  </article>
+
+                  <article className="projects__card">
+                     <img src={Project2} alt="" />
+                     <div className="projects__modal">
+                        <div>
+                           <span className="projects__subtitle">Web</span>
+                           <h3 className="projects__title">Portolio Website</h3>
+                           <a
+                              href="#"
+                              className="projects__button button button__small"
+                           >
+                              <i className="ri-link"></i>
+                           </a>
+                        </div>
+                     </div>
+                  </article>
+
+                  <article className="projects__card">
+                     <img src={Project3} alt="" />
+                     <div className="projects__modal">
+                        <div>
+                           <span className="projects__subtitle">Movil</span>
+                           <h3 className="projects__title">Fast food app</h3>
+                           <a
+                              href="#"
+                              className="projects__button button button__small"
+                           >
+                              <i className="ri-link"></i>
+                           </a>
+                        </div>
+                     </div>
+                  </article>
+
+                  <article className="projects__card">
+                     <img src={Project4} alt="" />
+                     <div className="projects__modal">
+                        <div>
+                           <span className="projects__subtitle">Design</span>
+                           <h3 className="projects__title">Music App Design</h3>
+                           <a
+                              href="#"
+                              className="projects__button button button__small"
+                           >
+                              <i className="ri-link"></i>
+                           </a>
+                        </div>
+                     </div>
+                  </article>
+               </div>
+
+               {/* skilss */}
+               <div className="skills__content grid " data-content id="skills">
+                  <div className="skills__area">
+                     <h3 className="skills__title">Frontend develper</h3>
+                     <div className="skills__box">
+                        <div className="skills__group">
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">HTML</h3>
+                                 <span className="skills__level">Basic</span>
+                              </div>
+                           </div>
+
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">React</h3>
+                                 <span className="skills__level">Advanced</span>
+                              </div>
+                           </div>
+
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">Css</h3>
+                                 <span className="skills__level">Avanced</span>
+                              </div>
+                           </div>
+                        </div>
+
+                        <div className="skills__group">
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">Tailwind</h3>
+                                 <span className="skills__level">
+                                    Intermediate
+                                 </span>
+                              </div>
+                           </div>
+
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">Git</h3>
+                                 <span className="skills__level">Advanced</span>
+                              </div>
+                           </div>
+
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">PostgreSQL</h3>
+                                 <span className="skills__level">Avanced</span>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div className="skills__area">
+                     <h3 className="skills__title">Backend develper</h3>
+                     <div className="skills__box">
+                        <div className="skills__group">
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">SQL</h3>
+                                 <span className="skills__level">
+                                    intermediate
+                                 </span>
+                              </div>
+                           </div>
+
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">Postgresql</h3>
+                                 <span className="skills__level">Advanced</span>
+                              </div>
+                           </div>
+
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">Firebase</h3>
+                                 <span className="skills__level">basic</span>
+                              </div>
+                           </div>
+                        </div>
+
+                        <div className="skills__group">
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">MySql</h3>
+                                 <span className="skills__level">Advanced</span>
+                              </div>
+                           </div>
+
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">NodeJS</h3>
+                                 <span className="skills__level">
+                                    Intermediate
+                                 </span>
+                              </div>
+                           </div>
+
+                           <div className="skills__data">
+                              <i className="ri-checkbox-circle-line"></i>
+                              <div className="">
+                                 <h3 className="skills__name">PostgreSQL</h3>
+                                 <span className="skills__level">Avanced</span>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </section>
       </main>
    );
