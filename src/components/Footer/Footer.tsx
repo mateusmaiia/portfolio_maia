28fea6d7bc363431fdb34730505250704f59b0ad
Maia <matteusmaia889@gmail.com>
2024-04-26 12:50:39 -0300

    feat: done footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+export function Footer() {
+  return (
+    <footer>
+      <span className="footer__copy">
+        &#169; Mateus Maia. All rights reserved.
+      </span>
+    </footer>
+  )
+}
