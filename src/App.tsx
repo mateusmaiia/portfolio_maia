f2118e5fefb89b6557e6d92f15602f259ceccea7
Maia <matteusmaia889@gmail.com>
2024-04-26 12:50:24 -0300

    feat: footer import

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import '../style.css';
-import { Header } from './components/Header/ Header';
-import { Main } from './components/Main/Main';
+import '../style.css'
+import '../dark-theme.js'
+import { Footer } from './components/Footer/Footer'
+import { Header } from './components/Header/ Header'
+import { Main } from './components/Main/Main'
 
 export function App() {
-   return (
-      <>
-         <Header />
-         <Main />
-      </>
-   );
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
 }
